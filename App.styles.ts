2862bf011150d71f1b8d59b348e9cb86df09c605
Rhea <101016117+StringRays@users.noreply.github.com>
2022-10-06 15:37:30 -0500

    Basic App

---
+++ App.styles.ts
@@ -0,0 +1,61 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import myBackground from './images/chalkboard.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+    }
+
+    body {
+        background: url(${myBackground});
+        background-size: cover;
+        margin: 0;
+        padding: 0 20px;
+        display: flex;
+        justify-content: center;
+    }
+
+    * {
+        box-sizing: border-box;
+        font-family: 'Open Sans', sans-serif; 
+    }
+`
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    line-items: center;
+
+    > p {
+        color: #fff;
+    }
+
+    .score {
+        color: #fff;
+        font-size: 2rem;
+        margin: 0;
+    }
+
+    h1 {
+        font-family: 'Special Elite', cursive;
+        color: #346B31; 
+        font-size: 70px;
+        text-align: center;
+        margin: 20px;
+    }
+
+    .start, .next {
+        cursor: pointer;
+        background: linear-gradient(180deg, #fff, #346B31);
+        border: 2px solid #346B31;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        height: 40px;
+        margin: 20px 0;
+        padding: 0 40px;    
+    }
+
+    .start {
+        max-width: 200px;
+    }
+`
---
+++ README.md
@@ -0,0 +1,1 @@
+### Quiz app built with React/Typescript
